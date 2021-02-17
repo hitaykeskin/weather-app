e11f56096ee4ee0c2ce372c8f42b51804d531f3c
hitayco <71069174+hitayco@users.noreply.github.com>
2021-02-17 22:47:52 +0300

    Updated HTML, CSS and JS

--- src/script.js
+++ src/script.js
@@ -1,5 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////////////////////
-//1) Tarihi yazıyoruz.
 let now = new Date();
 let days = [
   "Sunday",
@@ -36,9 +34,6 @@ function formatHours(timestrap) {
   return `${currentHour}:${currentMinute}`;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////
-//3)Call back function'ı yazıyoruz. event bir object'tir.event.preventDefault formun submit e-
-//dilmesini önler.Formun submit edilmesini engelliyoruz. h1 'i ve input bilgisini seçiyoruz.
 function handleSubmit(event) {
   event.preventDefault();
   let h1 = document.querySelector("h1");
@@ -47,13 +42,9 @@ function handleSubmit(event) {
   searchCity(cityInput.value);
 }
 
-//2) Form elementini seçiyoruz. Form'a submit eventListener'ını ekliyoruz.
 let searchForm = document.querySelector("form");
 searchForm.addEventListener("submit", handleSubmit);
 
-///////////////////////////////////////////////////////////////////////////////////////
-//5)retreiveTemperature fonks oluşturuyoruz. response bir object'tir. Tek tek html element
-//lerini seçiyoruz. Ardından object içindeki key-value pair'lerine eşitliyoruz
 function retrieveTemperature(response) {
   let temp = document.querySelector(".degree");
   let descr = document.querySelector(".description");
@@ -99,7 +90,7 @@ function displayForecast(response) {
           </div>`;
   }
 }
-//4) API call için searchCity fonksiyonunu oluşturduk. retrieveTemperature fonks ve retrieveForeccast oluşturacağız.
+
 function searchCity(city) {
   let apiKey = "8db434350a59b780ed9dec3c5447cf53";
   let units = "metric";
@@ -109,9 +100,7 @@ function searchCity(city) {
   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
 }
-///////////////////////////////////////////////////////////////////////////////////////
 
-//9)response
 function locationTemperature(response) {
   let temp = document.querySelector(".degree");
   let descr = document.querySelector(".description");
@@ -128,7 +117,7 @@ function locationTemperature(response) {
   wind.innerHTML = windSpeed;
   hum.innerHTML = humidity;
 }
-//8)API
+
 function getCurrentLocation(position) {
   let lat = Math.round(position.coords.latitude);
   let lon = Math.round(position.coords.longitude);
@@ -138,16 +127,13 @@ function getCurrentLocation(position) {
   let apiUrl = `${endPoint}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(locationTemperature);
 }
-//7)Lokasyon bilgisi allow deny penceresi için navigator.geolocation.getCurrentPosition
-//kullanıyoruz
+
 function getLocation() {
   navigator.geolocation.getCurrentPosition(getCurrentLocation);
 }
-//6) Button'u seçiyoruz. eventListener ekliyoruz. click ettiğinde getLocation fons. çalıştır!
+
 let currentLocationButton = document.querySelector(".current-location");
 currentLocationButton.addEventListener("click", getLocation);
-
-///////////////////////////////////////////////////////////////////////////////////////
 
 let celsiusTemperature = null;
 
@@ -175,4 +161,3 @@ let celsiusUnit = document.querySelector("#celsius-unit");
 celsiusUnit.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Istanbul");
-///////////////////////////////////////////////////////////////////////////////////////
